7aeaf30df73006b8ec0a6ad3a748154ba0bd5a88
ukrwebprom <ukrwebprom@gmail.com>
2023-05-25 12:31:25 +0300

    no alphanum password

--- middleware/validateSchemas.js
+++ middleware/validateSchemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const signup = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().alphanum().min(8).required()
+    password: Joi.string().min(8).required()
 });
 
 module.exports = {
